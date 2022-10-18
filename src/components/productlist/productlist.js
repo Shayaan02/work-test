d7fa707fc70211b249295c62db836b12398b93d4
Shayaan02 <shayaan.ahmed@gritacademy.se>
2022-10-18 20:28:06 +0200

    images

--- src/components/productlist/productlist.js
+++ src/components/productlist/productlist.js
@@ -5,21 +5,21 @@ export default class Products extends React.Component {
         return (
             <div className="Wrapper">
                 <ProductItem 
-                imageUrl="./images/pc1.jpg" imageAlt="pc" title="Lapotop" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" price="2259" discountedprice="1059" addToWishList={this.props.addToWishList}>
+                imageUrl="./work-test/images/pc1.jpg" imageAlt="pc" title="Lapotop" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" price="2259" discountedprice="1059" addToWishList={this.props.addToWishList}>
                 </ProductItem>
                 <ProductItem 
-                imageUrl="./images/gamingpc.webp" imageAlt="gamingpc" title="Gaming PC" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" price="22259" discountedprice="15259" addToWishList={this.props.addToWishList}>
+                imageUrl="./work-test/images/gamingpc.webp" imageAlt="gamingpc" title="Gaming PC" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" price="22259" discountedprice="15259" addToWishList={this.props.addToWishList}>
                 </ProductItem>
                 <ProductItem
-                 imageUrl="./images/barcelona.jpg" imageAlt="kit" title="Barcelona Kit 21/22" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" price="1259" discountedprice="999" addToWishList={this.props.addToWishList}>
+                 imageUrl="./work-test/images/barcelona.jpg" imageAlt="kit" title="Barcelona Kit 21/22" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" price="1259" discountedprice="999" addToWishList={this.props.addToWishList}>
                  </ProductItem>
                 <ProductItem
-                 imageUrl="./images/m2mbpro.jpg" imageAlt="macbook" title="Macbook Pro M2 Chip" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" discountedprice="13259" addToWishList={this.props.addToWishList}>
+                 imageUrl="./work-test/images/m2mbpro.jpg" imageAlt="macbook" title="Macbook Pro M2 Chip" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" discountedprice="13259" addToWishList={this.props.addToWishList}>
                  </ProductItem>
                 <ProductItem 
-                imageUrl="./images/mercedes.webp" imageAlt="mercedes" title="Mercedes-Benz S Klass" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" discountedprice="55259" addToWishList={this.props.addToWishList}>
+                imageUrl="./work-test/images/mercedes.webp" imageAlt="mercedes" title="Mercedes-Benz S Klass" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semiotics" discountedprice="55259" addToWishList={this.props.addToWishList}>
                 </ProductItem>
-                <ProductItem imageUrl="./images/Phantom.jpg" imageAlt="phantom" title="Rolls Royce Phantom" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semioticsbtitle" discountedprice="11000" addToWishList={this.props.addToWishList} >
+                <ProductItem imageUrl="./work-test/images/Phantom.jpg" imageAlt="phantom" title="Rolls Royce Phantom" subtitle="Lorem Ipsum Dolor Amet Offal Butcher Quinoa Sustainable Gastropub, Echo Park Actually Green Juice Sriracha Paleo. Brooklyn Sriracha Semioticsbtitle" discountedprice="11000" addToWishList={this.props.addToWishList} >
                 </ProductItem>
             </div>
         )
